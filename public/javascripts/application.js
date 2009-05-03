cec4e7410349287878a99876e1ef9b3d41af3914
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-03 20:40:08 +0100

    Show/hide Javascript is now properly unobtrusive.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function()
+{
+	TwitterEcho.initDifferentAccountLink();
+});
+
+var TwitterEcho = {};
+
+TwitterEcho.initDifferentAccountLink = function()
+{
+	$("#another_account")
+		.hide()
+		.before(
+
+			$("<a/>")
+				.append("use a different account?")
+				.attr("href", "#")
+				.click(function()
+				{
+					$("#another_account").slideToggle();
+					return false;
+				})
+
+		);
+}
